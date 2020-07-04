7ffb9188748253f1b0c715971b7cc20bf09f6530
CSeijiM <37257011+CSeijiM@users.noreply.github.com>
2020-07-04 00:09:39 -0300

    pid implented

--- Acelerometro/Debug/Core/Src/TJ_MPU6050.d
+++ Acelerometro/Debug/Core/Src/TJ_MPU6050.d
@@ -20,7 +20,9 @@ Core/Src/TJ_MPU6050.o: ../Core/Src/TJ_MPU6050.c ../Core/Inc/TJ_MPU6050.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
- ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
 
 ../Core/Inc/TJ_MPU6050.h:
 
@@ -67,3 +69,7 @@ Core/Src/TJ_MPU6050.o: ../Core/Src/TJ_MPU6050.c ../Core/Inc/TJ_MPU6050.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
 
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
